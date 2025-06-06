788273fbf2a1633a418c1799eef18955f5e5e0e4
Tom <tom@rocksalt.cc>
2025-06-06 11:38:31 +0100

    Updated create user script

--- scripts/create_user.ps1
+++ scripts/create_user.ps1
@@ -2,3 +2,14 @@ $Username = Read-Host "Enter Username"
 $Password = Read-Host -AsSecureString "Enter Password"
 
 New-LocalUser -Name $Username -Password $Password -FullName $Username -AccountNeverExpires
+
+Add-LocalGroupMember -Group "Users" -Member $Username
+Write-Host "User $Username created and added to Users group."
+
+$addUserToAdmins = Read-Host "Add user to admins? (Y/n)"
+if ($addUserToAdmins -ne "n" -and $addUserToAdmins -ne "N") {
+    Add-LocalGroupMember -Group "Administrators" -Member $Username
+    Write-Host "User $Username added to Administrators group."
+} else {
+    Write-Host "User $Username created without admin privileges."
+}
