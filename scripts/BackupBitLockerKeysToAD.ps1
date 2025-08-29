3fe48f675473af6ea8aa0cb198a35371aa953b97
Tom <tom@rocksalt.cc>
2025-08-29 09:56:19 +0100

    Added bitlocker backup to AD script

---
+++ scripts/BackupBitLockerKeysToAD.ps1
@@ -0,0 +1,8 @@
+﻿#Requires -RunAsAdministrator
+
+Get-BitLockerVolume | Where-Object {$_.ProtectionStatus -eq "On"} | ForEach-Object {
+ $MountPoint = $_.MountPoint
+ $_.KeyProtector | Where-Object {$_.KeyProtectorType -eq "RecoveryPassword"} | ForEach-Object {
+  Backup-BitLockerKeyProtector -MountPoint $MountPoint -KeyProtectorId $_.KeyProtectorId
+ }
+}
