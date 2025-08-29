ec668b9398d9921d39b8b8ccd35a6c9f7cf280c3
Tom <tom@rocksalt.cc>
2025-08-29 13:56:26 +0100

    Added logging to the bitlocker backup AD

--- scripts/BackupBitLockerKeysToAD.ps1
+++ scripts/BackupBitLockerKeysToAD.ps1
@@ -1,8 +1,10 @@
 ﻿#Requires -RunAsAdministrator
 
-Get-BitLockerVolume | Where-Object {$_.ProtectionStatus -eq "On"} | ForEach-Object {
- $MountPoint = $_.MountPoint
- $_.KeyProtector | Where-Object {$_.KeyProtectorType -eq "RecoveryPassword"} | ForEach-Object {
-  Backup-BitLockerKeyProtector -MountPoint $MountPoint -KeyProtectorId $_.KeyProtectorId
- }
+Get-BitLockerVolume | Where-Object { $_.ProtectionStatus -eq "On" } | ForEach-Object {
+  $MountPoint = $_.MountPoint
+  $_.KeyProtector | Where-Object { $_.KeyProtectorType -eq "RecoveryPassword" } | ForEach-Object {
+    Backup-BitLockerKeyProtector -MountPoint $MountPoint -KeyProtectorId $_.KeyProtectorId
+  }
 }
+
+Add-Content -Path "C:\Rocksalt\Bitlocker_Backup_Log.txt" -Value "$(Get-Date) : $env:USERNAME"
